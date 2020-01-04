1bacbfcbcb27bd4b9f1b66a9901a5d41cb1b0d69
QiFen.Pocket <QiFen.Pocket@gmail.com>
2020-01-04 15:28:04 +0800

    refactor: testcase

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -7,24 +7,56 @@ namespace DependencyInjectionWorkshopTests
     [TestFixture]
     public class AuthenticationServiceTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            _profileDao = Substitute.For<IProfile>();
+            _hash = Substitute.For<IHash>();
+            _logger = Substitute.For<ILogger>();
+            _otpService = Substitute.For<IOtpService>();
+            _notification = Substitute.For<INotification>();
+            _failedCounter = Substitute.For<IFailedCounter>();
+
+            _authenticationService =
+                new AuthenticationService(_profileDao, _hash, _otpService, _notification, _failedCounter, _logger);
+        }
+
+        private const string DefaultAccountId = "joey";
+        private const string DefaultHashedPassword = "hashed password";
+        private const string DefaultPassword = "1234";
+        private const string DefaultOtp = "otp";
+
+        private IProfile _profileDao;
+        private IHash _hash;
+        private ILogger _logger;
+        private IOtpService _otpService;
+        private INotification _notification;
+        private IFailedCounter _failedCounter;
+        private AuthenticationService _authenticationService;
+
+        private void GivenOtp(string accountId, string returnThis)
+        {
+            _otpService.CurrentOtp(accountId).Returns(returnThis);
+        }
+
+        private void GivenHashedPassword(string password, string hashedPassword)
+        {
+            _hash.ComputeHash(password).Returns(hashedPassword);
+        }
+
+        private void GivenPasswordFromDb(string accountId, string hashedPassword)
+        {
+            _profileDao.Password(accountId).Returns(hashedPassword);
+        }
+
         [Test]
         public void is_valid()
         {
-            var profile = Substitute.For<IProfile>();
-            var hash = Substitute.For<IHash>();
-            var logger = Substitute.For<ILogger>();
-            var otpService = Substitute.For<IOtpService>();
-            var notification = Substitute.For<INotification>();
-            var failedCounter = Substitute.For<IFailedCounter>();
+            GivenPasswordFromDb(DefaultAccountId, DefaultHashedPassword);
+            GivenHashedPassword(DefaultPassword, DefaultHashedPassword);
+            GivenOtp(DefaultAccountId, DefaultOtp);
 
-            profile.Password("joey").Returns("hashed password");
-            hash.ComputeHash("1234").Returns("hashed password");
-            otpService.CurrentOtp("joey").Returns("otp");
-
-            var authenticationService =
-                new AuthenticationService(profile, hash, otpService, notification, failedCounter, logger);
-
-            var valid = authenticationService.Verify("joey", "1234", "otp");
+            var valid = _authenticationService.Verify(DefaultAccountId, DefaultPassword, DefaultOtp);
 
             Assert.AreEqual(true, valid);
         }
